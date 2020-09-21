6b2f1700005f19b4e0b24e54d31dfe9e2b6544d1
Nathan Phillips <phillips4100@gmail.com>
2020-09-20 22:18:08 -0600

    connected title pragraph image and tabel through mongo.  still need to fix html table and image then add hemispheres.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -2,49 +2,45 @@ from bs4 import BeautifulSoup
 from splinter import Browser
 import pandas as pd
 import datetime as dt
+import time
 
-browser=Browser('chrome', executable_path="../chromedriver")
+executable_path = {'executable_path': '../chromedriver'}
+browser = Browser('chrome', **executable_path, headless=False)
 #NASA news Scrape
-def mars_news(browser):
+def mars_news():
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
+    time.sleep(1)
     html=browser.html
     NewsSoup = BeautifulSoup(html, 'html.parser')
-
-    try:
-        results = NewsSoup.find('div', class_="list_text")
-        Title = results.find('a').text
-        Pgrph = NewsSoup.find('div', class_="article_teaser_body").text
-    except AttributeError:
-        return None, None
-    return 'Title', 'Pgrph'
+    results = NewsSoup.find('div', class_="list_text")
+    Title = results.find('a').text
+    Pgrph = NewsSoup.find('div', class_="article_teaser_body").text
+    return Title, Pgrph
 
 # NASA JPL Site Scraper
 def featured_image_url():
-    # # open website and click full_image button
-    # JPL_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    # browser.visit(JPL_url)
-    # image = browser.find_by_id('full_image')
-    # image.click()
-    # more_info_element = browser.find_link_by_partial_text("more info")
-    # more_info_element.click()
-    # html = browser.html
-    # image_soup = BeautifulSoup(html, "html.parser")
-
-    # try:
-    #     image_url = image_soup.select_one("figure.lede a img").get("src")
-    # except AttributeError:
-    #     return None 
-    # featured_image_url = (f'https://www.jpl.nasa.gov{image_url}')
-    featured_image_url = 'https://pyxis.nymag.com/v1/imgs/115/983/a29c15a3a8d80f5a81d9c80799b74157d1-01-rbg.rhorizontal.w700.jpg'
+    # open website and click full_image button
+    JPL_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(JPL_url)
+    image = browser.find_by_id('full_image')
+    image.click()
+    time.sleep(.5)
+    more_info_element = browser.find_link_by_partial_text("more info")
+    more_info_element.click()
+    time.sleep(.5)
+    html = browser.html
+    image_soup = BeautifulSoup(html, "html.parser")
+    image_url = image_soup.select_one("figure.lede a img").get("src")
+    featured_image_url = (f'https://www.jpl.nasa.gov{image_url}')
+    # featured_image_url = 'https://pyxis.nymag.com/v1/imgs/115/983/a29c15a3a8d80f5a81d9c80799b74157d1-01-rbg.rhorizontal.w700.jpg'
     return featured_image_url
 
 # mars facts scrape
 def MarsFacts():
-    # mars_facts_url='https://space-facts.com/mars/'
-    # mars_data = pd.DataFrame(pd.read_html(mars_facts_url)[0])
-    # table = mars_data.to_html(header = False, index = False)
-    table = 'MESA GRANDE'
+    mars_facts_url='https://space-facts.com/mars/'
+    mars_data = pd.DataFrame(pd.read_html(mars_facts_url)[0])
+    table = mars_data.to_html(header = False, index = False)
     return table
 
 #mars Hemispheres
@@ -72,8 +68,8 @@ def scrape_all():
     # hemisphere_image_urls = hemisphere(browser)
 
     collection = {}
-    collection["title"]= Title
-    collection["pgrph"]= Pgrph
+    collection["title"]= mars_news()[0]
+    collection["pgrph"]= mars_news()[1]
     collection["img_url"]= featured_image_url()
     collection["table"]= MarsFacts()
     collection["date"]= dt.datetime.now()
